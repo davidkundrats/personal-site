c92ea808d3d63853bd50aeb291c172b1f4ec28ae
david kundrats <davidkundrats@gmail.com>
2024-01-03 19:32:59 -0500

    connect base template

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for siteproject project.
+URL configuration for blog project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings  # Import the settings module
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('base.urls')),
 ]
+
+# Serving static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
