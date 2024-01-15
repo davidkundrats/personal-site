5119b3f44ef9bc844f2f96a2ef24fc83fc761b06
david kundrats <davidkundrats@gmail.com>
2024-01-15 15:03:21 -0500

    junk

--- blog/settings.py
+++ blog/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h@%ce^y@dyqgi71#u1h22i8y4*=&yk$vm2kau@&1+e-c=8w3ur'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+
 
 
 # Application definition
